22acb6eabf93c518f156dfb5cf96f0397c42adb1
Amirhossein Khodabandehlou <amirho.kh97@gmail.com>
2019-02-18 21:48:16 +0330

    updating getCommand class to return animation attributes

--- Cpp Project ( BSL)/getCommand.cpp
+++ Cpp Project ( BSL)/getCommand.cpp
@@ -15,6 +15,42 @@ void getCommand::cmdRun(std::vector<std::string>& inputString, SVG & svg)
 	int keyLocation = inputString[1].find_last_of('>');
 	string key = inputString[1].substr(keyLocation + 1);
 
+	int arrowLocation1 = inputString[1].find_first_of('>');
+	int arrowLocation2 = inputString[1].find_last_of('>');
+	if (arrowLocation1 != arrowLocation2)
+	{
+		string animName = inputString[1].substr(arrowLocation1 + 1, ((arrowLocation2 - 2) - arrowLocation1));
+
+		for (auto shape : svg.getShape())
+		{
+			if (shapeName == shape->getName())
+			{
+				for (auto anim : shape->getAnimate())
+				{
+					if (animName == anim->getName())
+					{
+						for (auto att : anim->getAtt())
+						{
+							if (key == att->getKey())
+							{
+								cout << att->getValue() << endl;
+								return;
+							}
+						}
+					}
+
+
+				}
+
+
+			}
+
+		}
+
+
+
+
+	}
 	for (auto shape : svg.getShape())
 	{
 		if (shapeName == shape->getName())
