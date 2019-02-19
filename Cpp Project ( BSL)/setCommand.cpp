582d89e46a288c1b64cce771899f2b88d0d7fab5
Amirhossein Khodabandehlou <amirho.kh97@gmail.com>
2019-02-19 20:51:58 +0330

    updating setCommand class to support path features

--- Cpp Project ( BSL)/setCommand.cpp
+++ Cpp Project ( BSL)/setCommand.cpp
@@ -17,9 +17,24 @@ void setCommand::cmdRun(std::vector<std::string>& inputString, SVG &svg)
 	int valueLocation = inputString[2].find_last_of(')');
 	string value = inputString[2].substr(1, valueLocation - 1);
 
+	if (inputString.size() > 3)//to support path features
+	{
+		string pathValueStart = inputString[2].substr(1);
+		string pathValue;
+		pathValue += pathValueStart;
+		for (size_t i = 3; i <inputString.size(); i++)
+		{
+			pathValue += ' ';
+			pathValue += inputString[i];
+		}
+		pathValue.erase(pathValue.end());
+		value = pathValue;
+	}
+
 	Attribute* att = new Attribute;
 	att->setKey(key);
 	att->setValue(value);
+
 
 	int arrowLocation1 = inputString[1].find_first_of('>');
 	int arrowLocation2 = inputString[1].find_last_of('>');
